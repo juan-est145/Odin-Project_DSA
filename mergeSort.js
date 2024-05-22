54000e62513e4a935562d279d6d5d04675abc080
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-22 14:08:25 +0200

    Divide part of the algo is working

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,11 @@
 function mergeSort(array) {
-	
+	if (array.length > 1) {
+		let  mid = array.length / 2;
+		mergeSort(array.slice(0, mid));
+		mergeSort(array.slice(mid, array.length));
+	}
+	console.log(array);
 }
+
+mergeSort([105, 79, 100, 110]);
+
