cda13377ce7013d66edc3a41be9cb6246f26d406
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-22 21:03:22 +0200

    Size method is done

--- linkedLists.js
+++ linkedLists.js
@@ -8,11 +8,14 @@ class Node {
 class LinkedList {
 	constructor() {
 		this.head = null;
+		this.#listSize = 0;
 	}
+	#listSize;
 	append(value) {
 		if (this.head === null) {
 			this.head = new Node();
 			this.head.value = value;
+			this.#listSize++;
 			return;
 		}
 		let temp = this.head;
@@ -20,17 +23,23 @@ class LinkedList {
 			temp = temp.next;
 		temp.next = new Node();
 		temp.next.value = value;
+		this.#listSize++;
 	}
 	prepend(value) {
 		if (this.head === null) {
 			this.head = new Node();
 			this.head.value = value;
+			this.#listSize++;
 			return;
 		}
 		const newNode = new Node();
 		newNode.value = value;
 		newNode.next = this.head;
 		this.head = newNode;
+		this.#listSize++;
+	}
+	size() {
+		return (this.#listSize);
 	}
 	toString() {
 		let temp = this.head;
@@ -45,4 +54,13 @@ class LinkedList {
 }
 
 const head = new LinkedList();
+
+console.log(head.size());
+
+head.append(2);
+head.append(232);
+head.prepend("dasd");
+
+console.log(head.size());
+
 
