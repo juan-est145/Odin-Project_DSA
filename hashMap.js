a6d60edf857105e466c2496b1269634427479dd2
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-26 14:52:38 +0200

    Basic hash and set functionality is done

---
+++ hashMap.js
@@ -0,0 +1,40 @@
+class BucketNode {
+	constructor(key, value) {
+		this.#key = key;
+		this.value = value;
+		this.next = null;
+	}
+	#key;
+	getKey() {
+		return (this.#key);
+	}
+}
+
+class HashMap {
+	constructor() {
+		this.#array = new Array(16).fill(null);
+	}
+	#array;
+	hash(key) {
+		let hashCode = 0;
+		const primeNumber = 31;
+		for (let i = 0; i < key.length; i++) {
+			hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 16;
+		}
+		return hashCode;
+	}
+	set(key, value) {
+		let keyValue = this.hash(key);
+		if (this.#array[keyValue] === null) {
+			this.#array[keyValue] = new BucketNode(key, value);
+			return;
+		}
+		let temp = this.#array[keyValue];
+		while (temp.next != null && temp.getKey() !== key)
+			temp = temp.next;
+		if (temp.getKey() === key)
+			temp.value = value;
+		else
+			temp.next = new BucketNode(key, value);
+		}
+}
