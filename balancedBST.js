24e5d33b437b988816c8e438c4963e2b66721c5d
juestrel <128648875+juan-est145@users.noreply.github.com>
2024-05-27 16:10:09 +0200

    Delete item method is done

--- balancedBST.js
+++ balancedBST.js
@@ -48,19 +48,41 @@ class BinaryTree {
 		return (root);
 	}
 	deleteItem(value, root = this.root) {
-		if (root.data === value && root.left === null && root.right === null)
+		if (root === null)
+			return (null);
+		else if (root.data === value && root.left === null && root.right === null)
 			return (null);
 		else if (root.data === value) {
 			if (root.left !== null && root.right === null)
 				return (root.left);
 			else if (root.right !== null && root.left === null)
 				return (root.right);
+			root.data = this.minValue(root.right);
+			root.right = this.deleteItem(root.data, root.right);
 		}
 		else if (value < root.data)
 			root.left = this.deleteItem(value, root.left);
 		else if (value > root.data)
 			root.right = this.deleteItem(value, root.right);
 		return (root);
+	}
+	minValue(root = this.root) {
+		let temp = root;
+		let result = null;
+		while (temp !== null) {
+			result = temp.data;
+			temp = temp.left;
+		}
+		return (result);
+	}
+	maxValue(root = this.root) {
+		let temp = root;
+		let result = null;
+		while (temp !== null) {
+			result = temp.data;
+			temp = temp.right;
+		}
+		return (result);
 	}
 	find(value, root = this.root) {
 		if (root === null)
@@ -82,7 +104,7 @@ class BinaryTree {
 				queue.push(queue[0].left);
 			if (queue[0].right !== null)
 				queue.push(queue[0].right);
-			let currentNode = queue.shift(); 
+			let currentNode = queue.shift();
 			if (callback)
 				callback(currentNode);
 			else
@@ -96,7 +118,7 @@ class BinaryTree {
 //const bST = new BinaryTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 const bST = new BinaryTree([20, 40, 30, 50, 70, 80]);
 prettyPrint(bST.root);
-bST.deleteItem(80);
+bST.deleteItem(50);
 //console.log(bST.levelOrder());
 /*bST.levelOrder((item) => {
 	item.data *= 2;
