290ff80a5f720bd7ac5d819748adb59df6c6a9a0
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-05-28 12:28:44 +0200

    Driver script and Balanced BST are done

--- driverBST.js
+++ driverBST.js
@@ -1,4 +1,27 @@
-const {BinaryTree, prettyPrint} = require('./balancedBST');
+const { BinaryTree, prettyPrint } = require('./balancedBST');
 
-const myTree = new BinaryTree([32, 30, 40]);
+function randomArray(size = 11) {
+	return Array.from({ length: size }, () => Math.floor(Math.random() * 100));
+}
+
+const myTree = new BinaryTree(randomArray());
+const newValues = randomArray();
 prettyPrint(myTree.root);
+console.log(myTree.isBalanced());
+console.log(myTree.levelOrder());
+console.log(myTree.preOrder());
+console.log(myTree.postOrder());
+console.log(myTree.inOrder());
+newValues.forEach((item) => myTree.insert(item));
+prettyPrint(myTree.root);
+console.log(myTree.isBalanced());
+let isBalanced = myTree.isBalanced();
+if (isBalanced === false) {
+	myTree.rebalance();
+	prettyPrint(myTree.root);
+	console.log(myTree.isBalanced());
+}
+console.log(myTree.levelOrder());
+console.log(myTree.preOrder());
+console.log(myTree.postOrder());
+console.log(myTree.inOrder());
